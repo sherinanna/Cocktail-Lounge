1c50def41ca58dfecf65ded091360e0e1912ccb7
sherinsunny <sherinannasunny@gmail.com>
2022-06-08 20:39:55 -0500

    updated database_url for posgtress combatibility with sqlalchemy

--- app.py
+++ app.py
@@ -8,6 +8,7 @@
 
 import pdb
 import os
+import re
 from flask import Flask, flash,render_template, request,redirect,session,g
 from flask_debugtoolbar import DebugToolbarExtension
 from sqlalchemy.exc import IntegrityError
@@ -21,7 +22,8 @@ app = Flask(__name__)
 # Get DB_URI from environ variable (useful for production/testing) or,
 # if not set there, use development local db.
 app.config['SQLALCHEMY_DATABASE_URI'] = (os.environ.get('DATABASE_URL', 'postgresql:///cocktail'))
-
+if app.config['SQLALCHEMY_DATABASE_URI'].startswith("postgres://"):
+    app.config['SQLALCHEMY_DATABASE_URI'] = app.config['SQLALCHEMY_DATABASE_URI'].replace("postgres://", "postgresql://", 1)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'shh')
